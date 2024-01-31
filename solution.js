a4cd36f03eb5d2106f18b41364c92ef308c577cb
zynbahmed <201901241@student.polytechnic.bh>
2024-01-31 09:09:20 +0300

    question 7

--- solution.js
+++ solution.js
@@ -130,6 +130,16 @@ console.log("Result 6:", calculateAverage(numArr4))  // Expected output is 30
 
 const isPalindrome = (str) => {
     // Your solution here 
+    const cleanStr = str.replace(/[^a-z]/g, '').toLowerCase()
+
+  // Compare characters from start and end
+  for (let i = 0; i < Math.floor(cleanStr.length / 2); i++) {
+    if (cleanStr[i] !== cleanStr[cleanStr.length - 1 - i]) {
+      return false
+    }
+  }
+
+  return true
 }
 
 // Test
